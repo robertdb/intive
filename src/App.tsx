44d60a70e7f88e21a668c798e667aafe6c328e93
Robert Diaz Bejar <robertdibejar@gmail.com>
2019-05-11 10:46:31 -0300

    files unnecessary, have been deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div>
+        <p>
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
